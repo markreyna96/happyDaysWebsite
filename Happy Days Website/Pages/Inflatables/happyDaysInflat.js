d21692979863e1f9815dfe22c58289f6bc1adb5a
Mark Reyna <MarkReyna@wifi-help-10-90-4-116.public.utexas.edu>
2018-11-19 13:35:41 -0600

    updates

--- Happy Days Website/Pages/Inflatables/happyDaysInflat.js
+++ Happy Days Website/Pages/Inflatables/happyDaysInflat.js
@@ -1,17 +1,38 @@
-var subMenuState = [false,false,false];
 
 function inflatSubClick(something){
+	//an array containing each subtab's id
 	var blockArray = ["moonjumpBlock","waterslideBlock","interactivesBlock"];
-	console.log(subMenuState);
-	var gain = Number(something);
-	subMenuState[something] = !subMenuState[something];
-	console.log(subMenuState);
+	console.log(something);
+	//for loop that sets the display setting of all elements to "none"
+	var mjBlock = document.getElementsByClassName("moonjumpBlock");
+	console.log(mjBlock);
+	for(i=0;i<mjBlock.length;i++){
+		// console.log(i);
+		// console.log(blockArray[i]);
+		mjBlock[i].style.display = "none";
+	}
 
-	if (subMenuState[something]){
-		document.getElementById(blockArray[something]).style.display = "none";
+	var wsBlock = document.getElementsByClassName("waterslideBlock");
+	console.log(wsBlock);
+	for(i=0;i<wsBlock.length;i++){
+		// console.log(i);
+		// console.log(blockArray[i]);
+		wsBlock[i].style.display = "none";
 	}
-	else{
-		document.getElementById(blockArray[something]).style.display = "block"; 
+
+	var iaBlock = document.getElementsByClassName("interactivesBlock");
+	console.log(iaBlock);
+	for(i=0;i<iaBlock.length;i++){
+		// console.log(i);
+		// console.log(blockArray[i]);
+		iaBlock[i].style.display = "none";
 	}
-	subMenuState
+
+	//the element selected has its display setting changed to "block"
+	var makeTrue = document.getElementsByClassName(blockArray[something]);
+	for(i=0;i<makeTrue.length;i++){
+		// console.log(i);
+		// console.log(blockArray[i]);
+		makeTrue[i].style.display = "block";
+	}
 }
